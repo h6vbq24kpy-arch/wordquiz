d47c97681b5c6755c1f43ed43892d99d87c8d0ac
Takeo <h6vbq24kpy@privaterelay.appleid.com>
2026-01-08 21:56:41 +0900

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,27 @@
+//HTMlの要素の呼び出し
+const answer1 = document.querySelector('.Correct');
+const answer2 = document.querySelector('.Notcorrect1');
+const answer3 = document.querySelector('.Notcorrect2');
+const result = document.querySelector('.judge');
+const CurrentPage = document.querySelector('.pages');
+
+answer1.addEventListener('click', () => {
+    const showresult = '×不正解です';
+
+    result.innerHTML = showresult;   
+});
+
+answer2.addEventListener('click', () => {
+    const showresult2 = '◎正解です';
+
+    result.innerHTML = showresult2;
+});
+
+answer3.addEventListener('click', () => {
+    const showresult3 = '×不正解です';
+
+    result.innerHTML = showresult3;
+});
+
+
+
